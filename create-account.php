cbf1304f631f751f5bcafd86e1c7e726791045f5
wangshenbao <wangshenbao.ben@gmail.com>
2016-12-21 05:55:52 +0000

    Php create acc finished

--- create-account.php
+++ create-account.php
@@ -9,6 +9,7 @@
 	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
 	<link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css">
 	<link href="css/templatemo_style.css" rel="stylesheet" type="text/css">	
+    <script type="text/javascript" src="create-account.php"></script>
 </head>
 <body class="templatemo-bg-gray">
 	<h1 class="margin-bottom-15">Create Account</h1>
@@ -19,23 +20,23 @@
 					<div class="form-group">
 			          <div class="col-md-6">		          	
 			            <label for="first_name" class="control-label">First Name</label>
-			            <input type="text" class="form-control" id="first_name" placeholder="">		            		            		            
+			            <input type="text" class="form-control" id="first_name" placeholder=""name="first_name">		            		            		            
 			          </div>  
 			          <div class="col-md-6">		          	
 			            <label for="last_name" class="control-label">Last Name</label>
-			            <input type="text" class="form-control" id="last_name" placeholder="">		            		            		            
+			            <input type="text" class="form-control" id="last_name" placeholder=""name="last_name">		            		            		            
 			          </div>             
 			        </div>
 			        <div class="form-group">
 			          <div class="col-md-12">		          	
 			            <label for="username" class="control-label">Email</label>
-			            <input type="email" class="form-control" id="email" placeholder="">		            		            		            
+			            <input type="email" class="form-control" id="email" placeholder=""name="email">		            		            		            
 			          </div>              
 			        </div>			
 			        <div class="form-group">
 			          <div class="col-md-6">		          	
 			            <label for="username" class="control-label">Username</label>
-			            <input type="text" class="form-control" id="username" placeholder="">		            		            		            
+			            <input type="text" class="form-control" id="username" placeholder=""name="username">		            		            		            
 			          </div>
 			          <div class="col-md-6 templatemo-radio-group">
 			          	<label class="radio-inline">
@@ -49,11 +50,11 @@
 			        <div class="form-group">
 			          <div class="col-md-6">
 			            <label for="password" class="control-label">Password</label>
-			            <input type="password" class="form-control" id="password" placeholder="">
+			            <input type="password" class="form-control" id="password" placeholder=""name="password">
 			          </div>
 			          <div class="col-md-6">
 			            <label for="password" class="control-label">Confirm Password</label>
-			            <input type="password" class="form-control" id="password_confirm" placeholder="">
+			            <input type="password" class="form-control" id="password_confirm" placeholder=""name="password_confrim">
 			          </div>
 			        </div>
 			        <div class="form-group">
@@ -92,5 +93,69 @@
 	</div>
 	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    
+    
+    <?php
+
+if(isset($_POST['first_name']))
+{
+$dbhost = 'localhost';
+$dbuser = 'project';
+$dbpass = 'root';
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, 'event_manager');
+if(! $conn )
+{
+  die('Could not connect: ' . mysql_error());
+}
+
+
+
+$first_name=$_POST["first_name"];
+   
+$last_name=$_POST["last_name"];
+    
+$email=$_POST["email"];
+     
+$username=$_POST["username"];
+     
+$password=$_POST["password"];
+    
+    
+$password_confirm=$_POST["password_confrim"];
+
+if($password !=$password_confirm)
+{
+echo "Password is not match!";
+}
+    
+$sql = "INSERT INTO user_info ".
+       "(firstname,lastname,email,username,password) ".
+       "VALUES ".
+       "('$first_name','$last_name','$email','$username','$password')";
+ $result = mysqli_query($conn, $sql );   
+    echo"$result";
+if( !$result)
+{?>
+  <script>
+   alert("error\n");    
+    </script>   
+    <?php
+}
+    else{
+        
+    
+
+    mysqli_close($conn);
+?>
+    <script>
+alert("Entered data successfully\n");    
+    </script>
+    <?php
+    }
+}
+      ?>
 </body>
+
+
 </html>
