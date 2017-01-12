6dbe625294651f984677f47267ccb97fad98617c
wangshenbao <wangshenbao.ben@gmail.com>
2017-01-12 23:14:44 +0000

    Explain
    explain the mail code logically.

--- controllers/book-tickets.php
+++ controllers/book-tickets.php
@@ -76,18 +76,18 @@ $dbuser = 'project';
 $dbpass = 'root';
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, 'event_manager'); 
     
-    $sql_u ="SELECT * FROM `events` WHERE `id`='$event_id' "; 
+    $sql_u ="SELECT * FROM `events` WHERE `id`='$event_id' "; // getting data from database
 
      $result_u = mysqli_query($conn, $sql_u );
      $array=mysqli_fetch_array($result_u,MYSQLI_NUM);  
      mysqli_free_result($result_u);
-    $event=$array[2];
-     $lefttime=$array[6]-time();
+    $event=$array[2]; // event name
+     $lefttime=strtotime($array[6])-time();//  time（） is the current time when user book the event.  $array[6] is the event start time.
     if($lefttime>=86400){
-        $interval=$lefttime-86400;
+        $interval=$lefttime-86400;     // if the lefttime larger than 86400 which means it is more than one day. so we need ask php code sleep for these time until it is one day before the event
     }
     else{
-        $interval=0;
+        $interval=0;     //else intervel =0 .so in check_send function, the email will send immediately. 
     }
     
     
@@ -103,7 +103,7 @@ $from = "wangshenbao.ben@gmail.com";
 $headers = "From: $from";
 mail($to,$subject,$message,$headers);
 
-
+// send a email//
   
     
     
@@ -114,11 +114,11 @@ mail($to,$subject,$message,$headers);
          send();
      }
      else{
-         ignore_user_abort(true); 
-         set_time_limit(0); 
-         sleep($interval);
-         send();
-         ignore_user_abort(false);
+         ignore_user_abort(true); // php still working in webserve after user close the window
+         set_time_limit(0); // continue do php for infinite time
+         sleep($interval); // sleep for some time 
+         send();//send email
+         ignore_user_abort(false);// stop the php
      }
  }   
     
